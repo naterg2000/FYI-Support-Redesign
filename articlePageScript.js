37f56baa033231d16232eaca6a7cede8cc255626
Nathan Goshay <naterg2000@gmail.com>
2023-08-10 01:22:01 -0700

    add toggleable article section container

---
+++ articlePageScript.js
@@ -0,0 +1,7 @@
+function showDiv() {
+    document.getElementById('article-detail-container').style.display = "block";
+ }
+
+function closeArticleDetailSection() {
+    document.getElementById('article-detail-container').style.display = "none";
+}
